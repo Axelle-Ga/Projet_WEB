f2b81d9c8e04976184833daf1fcae2fa1a1ca28f
Axelle Gaige <axelle.gaige@gmail.com>
2020-11-07 18:54:28 +0100

    Ajout Carte

---
+++ map.js
@@ -0,0 +1,16 @@
+var map;
+
+function initMap(){
+  map= L.map('map').fitWorld({animate:false});
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      minZoom:1,
+      maxZoom: 18,
+      id: 'mapbox/satellite-v9',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoiYWdhaWdlIiwiYSI6ImNrZ2huODE5YjBnMGoyc3RldGZkZThxcHgifQ.Hwxwc-MYlZ01QTPAIFSloQ'
+  }).addTo(map);
+}
+
+initMap();
