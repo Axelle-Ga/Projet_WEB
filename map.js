647b8f40826d6dbcbbc703a93556ad09bc8cd11e
Axelle Gaige <axelle.gaige@gmail.com>
2020-11-13 11:19:30 +0100

    ZOOM OK

--- map.js
+++ map.js
@@ -1,4 +1,5 @@
 var mymap;
+
 /* initMap met en place le fond de carte en spécifiant les limites de zooms possibles et,
 bien entendu, la source du fond de carte désiré, ici, OpenStreetMap. */
 function initMap(){
@@ -11,83 +12,53 @@ function initMap(){
   }
 
 
-initMap();
-
 /* Ici, on récupère le fichier php et on analyse la base de données des objets pour leur affichage dans la couche layer. */
-let layer = new L.featureGroup();
 
-var objets;
-var type1 = [];
-var type2 = [];
-var type3 = [];
-var type4 = [];
-var type5 = [];
+
+
 
 fetch('map.php', {
 	method: 'post',
   })
   .then(r => r.json())
   .then(r => {
+	var layer = new L.featureGroup();
+	var objets=r;
+	var zoomMin =[];
 	console.log(r)
-	objet=r;
+	initMap();
 	r.forEach(element => {
 		var ico = L.icon({iconUrl : element.icone, iconAnchor : [76,189], popupAnchor : [0,-175]}); /* Récupération de l'icône et ancrage. */
 		var mark = L.marker([element.latitude, element.longitude],{icon: ico}, {title :element.nom}); /* Placement de l'icône. */
 		mark.bindPopup(element.indice);
+		
 		layer.addLayer(mark);
-		console.log(layer.getLayerId(mark));
-		if (element.type==1){
-			type1.push(layer.getLayerId(mark));
-		}
-		if (element.type==2){
-			type2.push(layer.getLayerId(mark));
-		}
-		if (element.type==3){
-			type3.push(layer.getLayerId(mark));
-		}
-		if (element.type==4){
-			type4.push(layer.getLayerId(mark));			
-		}
-		if (element.type==5){
-			type5.push(layer.getLayerId(mark));
-		}
+
+		zoomMin.push(element.minZoom);
 	});
+	mymap.addEventListener("zoomend",function() {onZoom(mymap,zoomMin,layer)},true);
   })
 
-  
-  mymap.on("zoomend",onZoom);
 
-  function onZoom(event){
-	if (mymap.getZoom()<5){
-		console.log(mymap.getZoom());
-		layer.remove();
-	}
-	else if (mymap.getZoom()>=5){
-		console.log(mymap.getZoom());
-		mymap.addLayer(layer);
+  function onZoom(mymap, zoomMin, layer){
+	var displayed = layer.getLayers();
+	console.log(mymap.getZoom());
+	for (var i = 0 ; i < displayed.length ; i++){
+		if (mymap.hasLayer(displayed[i]) && mymap.getZoom()<zoomMin[i]){
+			console.log("remove");
+			mymap.removeLayer(displayed[i]);
+		}
+		else if (!(mymap.hasLayer(displayed[i])) && (mymap.getZoom()>=zoomMin[i])){
+			displayed[i].addTo(mymap);
+		}
 	}
   }
 
-  layer.on("click", onClick);
+ /* layer.on("click", onClick);
 
   function onClick(event){
 	console.log(event);
-	if (type1.includes(event.layer._leaflet_id)){
-		
-	}
-	if (type2.includes(event.layer._leaflet_id)){
-		prompt("Quel est le code?")	
-	}
-	if (type3.includes(event.layer._leaflet_id)){
-		alert("Le code est")
-	}
-	if (type4.includes(event.layer._leaflet_id)){
-		
-	}
-	if (type5.includes(event.layer._leaflet_id)){
-		
-	}	
-  }
+  }*/
 
 
   
