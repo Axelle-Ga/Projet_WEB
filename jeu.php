69ef46dd414544f8155a3c2b1274d4669d03f5e7
Axelle Gaige <axelle.gaige@gmail.com>
2020-11-14 23:27:00 +0100

    Bag & chrono style

--- jeu.php
+++ jeu.php
@@ -11,10 +11,12 @@
   <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
   crossorigin=""></script>
+  <script src="https://kit.fontawesome.com/aed932af5e.js" crossorigin="anonymous"></script>
   </head>
 
   <body>
-      <h1>GéoEscape</h1>
+    <div class="jeu">
+      <h2>GéoEscape</h2>
       <div id = map></div>
       <div id = "sac">
           <div class = "poche"></div>
@@ -22,5 +24,14 @@
           <div class = "poche"></div>
           <div class = "poche"></div>
       </div>
-      <script type="text/javascript" src="map.js"></script>
+      <div id = 'timer'>
+        <span id = "temps" style="font-size: 2.5rem;">
+          <span style="color: #FB8500;">
+            <i class="fas fa-stopwatch"></i>
+          </span>
+          <span id = 'chrono'>00:00:00</span>
+        </span>
+      </div>
+    </div>
+    <script type="text/javascript" src="map.js"></script>
   </body>
