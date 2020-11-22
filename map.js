f302933d7ecf71b6e4fbae7711fc039199375be1
Axelle Gaige <axelle.gaige@gmail.com>
2020-11-22 20:57:26 +0100

    Commentaires

--- map.js
+++ map.js
@@ -12,11 +12,7 @@ function initMap(){
   }
 
 
-/* Ici, on récupère le fichier php et on analyse la base de données des objets pour leur affichage dans la couche layer. */
-
-
-
-
+//on va créer la carte de base en récupérant les objets dans la base de donnée
 fetch('map.php', {
 	method: 'post',
   })
@@ -32,14 +28,9 @@ fetch('map.php', {
 	r.forEach(element => {
 		var ico = L.icon({iconUrl : element.icone, iconAnchor : [76,189], popupAnchor : [0,-175], id : element.id}); /* Récupération de l'icône et ancrage. */
 		var mark;
-		if (element.type == 4){
-			/* Placement de l'icône. */
-			mark = L.marker([element.latitude, element.longitude],{icon: ico}, {title :element.nom}, {draggable: true}); 
-		}
-		else{
-			/* Placement de l'icône. */
-			mark = L.marker([element.latitude, element.longitude],{icon: ico}, {title :element.nom}); 
-		}
+		/* Placement de l'icône. */
+		mark = L.marker([element.latitude, element.longitude],{icon: ico}, {title :element.nom}); 
+
 		mark.bindPopup(element.indice);
 		
 		layer.addLayer(mark);
@@ -53,62 +44,106 @@ fetch('map.php', {
 	});
 
 
-	layer.on("click", function() {onClick(event, visible,layer, objets, id )});
+	layer.on("click", function() {onClick(event, visible,layer, objets, id, zoomMin )});
 	mymap.addEventListener("zoomend",function() {onZoom(mymap,zoomMin,visible,layer, objets,id)},true);
+	
 	
   })
 
 
   function onZoom(mymap, zoomMin,visible, layer, objets,id){
+	//en fonction du zoom de la carte les marker sont affichés ou non
+
+	//On récupère les couches
 	var displayed = layer.getLayers();
+	//Pour chaque couche on reagrde si elle doit être affichée ou non
 	for (var i = 0 ; i < displayed.length ; i++){
+
+		//Si a couche i est affichée mais que le zoom est trop petit
 		if (mymap.hasLayer(displayed[i]) && mymap.getZoom()<zoomMin[i]){
+			//On enlève la couche
 			mymap.removeLayer(displayed[i]);
 		}
+
+		//Si la couche n'est pas affichée alors qu'elle doit l'être (zoom et visbilité ok)
 		else if (!(mymap.hasLayer(displayed[i])) && (mymap.getZoom()>=zoomMin[i]) && visible[i]==1){
+			//On rajoute la couche
 			displayed[i].addTo(mymap);
 		}
 	}
   }
 
 
-  function onClick(event, visible, layer, objets, id){
+  function onClick(event, visible, layer, objets, id, zoomMin){
 	
+	var displayed = layer.getLayers();
+
 	var url = event.explicitOriginalTarget.attributes.src.nodeValue;
 	var num = id.indexOf(url);
 	console.log(num);
 	console.log(id);
 	console.log(objets);
-	var music = new Audio(objets[num].music);
+	console.log(event.target.src)
+
+	/*var music = new Audio(objets[num].music);
 	music.load();
-	music.play();
+	music.play();*/
+
+	//objet basique qui libère le suivant mais reste sur la carte
 	if (objets[num].type ==1) {
 		visible[num+1]=1;
+		if (!(mymap.hasLayer(displayed[num+1])) && (mymap.getZoom()>=zoomMin[num+1]) && visible[num+1]==1){
+		displayed[num+1].addTo(mymap);
+		}
 	}
+	//objets de type coffre-fort à débloquer par code
 	else if (objets[num].type ==2){
 		visible[num+1]=1;
-		code(objets, num, visible);
+		if (!(mymap.hasLayer(displayed[num+1])) && (mymap.getZoom()>=zoomMin[num+1]) && visible[num+1]==1){
+		displayed[num+1].addTo(mymap);
+		}
+		code(objets, num, visible, displayed);
 	}
+	//objet de type récupérable
 	else if (objets[num].type ==4){
+		visible[num]=0;
+		mymap.removeLayer(displayed[num]);
 
 	}
+	//objet bloqué par un autre objet
 	else if (objets[num].type ==5){
 
 	}
+	//objet dont on pourra lire le pop-up et qui disparrai après pour laisser place à un autre
+	else if (objets[num].type ==6){
+		visible[num]=0;
+		visible[num+1]=1;
+		displayed[num+1].addTo(mymap);
+		mymap.removeLayer(displayed[num]);
+	}
   }
 
-  function code(objets,num, visible) {
-	console.log(objets[num].code);
+
+
+
+
+  
+  function code(objets,num, visible, displayed) {
+	//améliorer cette fonction pour quel fonctionne avec un formulaire dans le pop-up
+
 	let result = prompt("Quel est le code?");
-	console.log(result);
+
+	//si c'est le bon code on libère l'objet suivant et on supprime le coffre
 	if (parseInt(result) == objets[num].code) {
 		alert("C'est le bon code!!!");
 		visible[num+2]=1;
+		visible[num]=0;
+		mymap.removeLayer(displayed[num]);
+		displayed[num+2].addTo(mymap);
 	}
+	//si ce n'est pas le bon code on previent le joueur
 	else{
 		alert("ERREUR : le code est faux")
 	}
   }
-
-
-  
+	
