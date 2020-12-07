b30b88d95fae546ed4373fec1d1051a865b69c67
Leomazel <leo.mazel@ensg.eu>
2020-12-07 19:49:26 +0100

    Affichage highscore

--- score.js
+++ score.js
@@ -1,9 +1,15 @@
-let rang=[r1,r2,r3,r4,r5];
+let nom_rang=['r1','r2','r3','r4','r5'];
+let rang=[];
+for (var i = 0; i<nom_rang.length;i++){
+  rang.push(document.getElementById(nom_rang[i]));
+};
 fetch('scoreboard.php', {
-	method: 'post',
+    method: 'post',
   })
   .then(r => r.json())
   .then(r => {
-		r.forEach(element => {
-			console.log("salut");
-		})
+    console.log(r);
+        for (var i = 0; i<r.length;i++) {
+            rang[i].innerHTML=r[i].username+' '+r[i].time;
+        };
+    })
--- scoreboard.php
+++ scoreboard.php
@@ -1,7 +1,7 @@
 <?php
 $link = mysqli_connect('localhost', 'root', 'root', 'highscore');
 
-if($result = mysqli_query($link, 'SELECT * FROM scores ORDER BY time DESC LIMIT 5')){
+if($result = mysqli_query($link, 'SELECT * FROM scores ORDER BY time ASC LIMIT 5')){
 
     while($pinpoint = mysqli_fetch_assoc($result)) {
 
