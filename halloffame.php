29d395fb1c585e2c0f91a9cd8576c6e51ef202ab
Leomazel <leo.mazel@ensg.eu>
2020-12-14 18:45:19 +0100

    création pages apropos halloffame + css modifié

---
+++ halloffame.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <title>EscapeGame géographique</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" media="screen" href="style.css">
+    <link rel="icon" type="image/png" href="img/favicon.png">
+  </head>
+
+  <body>
+    <a href="index.php" title="Accueil"><i id = "home" class="fas fa-home"></i></a>
+    <div class="cadreContainer">
+      <div class = "cadre">
+        <h1>L'affaire Müller</h1>
+
+        <div class = "hall">
+          <h3>Meilleurs scores :</h3>
+          <ol>
+          <li id="r1"></li>
+          <li id="r2"></li>
+          <li id="r3"></li>
+          <li id="r4"></li>
+          <li id="r5"></li>
+        </ol>
+        </div>
+      </div>
+    </div>
+  <script src="score.js"></script>
+  </body>
