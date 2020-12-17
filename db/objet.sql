1aead6b93a739b5e517680c4ffb57d5dd2d91074
Axelle Gaige <axelle.gaige@gmail.com>
2020-12-17 12:31:55 +0100

    Nouvelle db

--- db/objet.sql
+++ db/objet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 12, 2020 at 07:32 PM
+-- Generation Time: Dec 17, 2020 at 11:30 AM
 -- Server version: 5.7.24
 -- PHP Version: 7.4.1
 
@@ -48,7 +48,39 @@ INSERT INTO `scores` (`username`, `time`) VALUES
 ('Axou', '00:00:28'),
 ('Salut', '00:00:09'),
 ('Axelle', '00:01:26'),
-('Charlotte', '00:01:27');
+('Charlotte', '00:01:27'),
+('Axelle', '00:01:41'),
+('Axelle', '00:01:41'),
+('Axelle', '00:01:41'),
+('Axelle', '00:01:41'),
+('Axelle', '00:01:41'),
+('Axelle', '00:01:41'),
+('Axland', '00:01:20'),
+('Axelle', '00:01:18'),
+('Axlandette', '00:01:09'),
+('Axlandette', '00:01:09'),
+('Axlandette', '00:01:09'),
+('Axlandette', '00:01:09'),
+('Axlandette', '00:01:09'),
+('5130', '00:01:03'),
+('Axelle', '00:00:59'),
+('Axelle', '00:01:31'),
+('Julie', '00:02:25'),
+('Axelle', '00:01:08'),
+('Axelle', '00:01:08'),
+('Axelle3456', '00:01:08'),
+('Xouille', '00:01:05'),
+('Xouille', '00:01:05'),
+('Faux_Axthéli', '00:01:52'),
+('Faux_Axthéli', '00:01:52'),
+('Axelle', '00:01:14'),
+('Axelle51', '00:01:42'),
+('Axelle', '00:00:00'),
+('Axelle', '00:00:00'),
+('Axelle', '00:00:00'),
+('Axelle', '00:01:06'),
+('Axelle', '00:02:03'),
+('Axland', '00:01:06');
 
 -- --------------------------------------------------------
 
@@ -89,12 +121,12 @@ INSERT INTO `tbl_objet` (`id`, `nom`, `texte`, `indice`, `latitude`, `longitude`
 (5, 'Image', 'Sur les images vous voyez une femme quitter le logement de Markus Muller. Elle tient un bout de papier qui semble être un billet d’avion.\r\n<p>Ne perdez pas de temps!</p>\r\nRécupérez votre billet et allez à l\'aéroport de Salzburg pour prendre le premier vol direction l\'Egypte.\r\n\r\n<p id =\'indice_texte\' style=\'text-align:center;\'><button class = \'bouton_indice\'>Indice</button> </p>', '<p>L\'aéroport se trouve à l\'ouest de Salzburg.</p>', 47.8621, 13.1253, 'img/bobine.png', 100, 92, 'music/zapsplat_horror_stab_dark_scary_cinematic_004_11803.mp3', 1, 17, 0, NULL, 6, NULL, NULL),
 (6, 'Billet', 'Votre inventaire est plein. Résolvez les énigmes précédentes avant de récupérer cet objet.', '', 47.8622, 13.1262, 'img/billet.png', 232, 97, 'music/zapsplat_horror_stab_dark_scary_cinematic_004_11803.mp3', 4, 16, 0, NULL, 7, NULL, NULL),
 (7, 'Aéroport', 'Une femme correspondant à celle des images de surveillance à pris l’avion pour l’Egypte la nuit du meurtre. Vous la retrouverez peut-être là-bas.\r\n\r\n<p id =\'indice_texte\' style=\'text-align:center;\'><button class = \'bouton_indice\'>Indice</button> </p>', '<p>Utiliser votre billet pour prendre l\'avion.</p>', 47.7942, 13.0012, 'img/avion.png', 300, 75, 'music/zapsplat_horror_stab_dark_scary_cinematic_004_11803.mp3', 5, 16, 0, NULL, 9, NULL, 6),
-(8, 'Madame', 'Bravo ! Vous avez retrouvé la meurtrière !<form id = \"form\" action=\"fin.php\" method=\'post\'>\r\n<input type=\"hidden\" name=\"time\" value=\"\" >\r\n<input type=\"texte\" name=\"username\" placeholder=\"Username\" required><br>\r\n<button type=\"submit\" name=\"submit\">Submit</button>\r\n</form>', '', 29.9773, 31.1303, 'img/madame.png', 101, 144, 'music/zapsplat_horror_stab_dark_scary_cinematic_004_11803.mp3', 6, 7, 0, NULL, NULL, NULL, NULL),
+(8, 'Madame', 'Bravo ! Vous avez retrouvé la meurtrière !<form id = \"form\" action=\"fin.php\" method=\'post\' style=\'text-align:center;\'>\r\n<input type=\"hidden\" name=\"time\" value=\"\" >\r\n<input type=\"texte\" name=\"username\" placeholder=\"Username\" required><br>\r\n<p>\r\n<button type=\"submit\" name=\"submit\">Submit</button>\r\n</p>\r\n</form>', '', 29.9773, 31.1303, 'img/madame.png', 101, 144, 'music/zapsplat_horror_stab_dark_scary_cinematic_004_11803.mp3', 6, 7, 0, NULL, NULL, NULL, NULL),
 (9, 'antiquaire', 'Vous interrogez l\'antiquaire de l\'aéroport, mais il ne répondra à vos question que si vous l\'aider à retrouver les vases canope qui lui ont été volés.\r\n\r\n<p>Rapportez lui les vases dans le bon ordre car il est un peu maniaque </p>\r\n\r\n<p id =\'indice_texte\' style=\'text-align:center;\'><button class = \'bouton_indice\'>Indice</button> </p>', 'Les vases se trouvent dans les alentours. Observez les biens pour déterminer le bon ordre.', 30.1123, 31.397, 'img/antiquaire.png', 57, 111, NULL, 5, 14, 0, NULL, 10, NULL, 17),
 (10, 'Antiquaire 1', 'L\'antiquaire attend le deuxième vase canope. <p id =\'indice_texte\' style=\'text-align:center;\'><button class = \'bouton_indice\'>Indice</button> </p>', 'Le deuxième vase a une tête de chacal.', 30.1123, 31.397, 'img/antiquaire1.png', 77, 111, NULL, 5, 14, 0, NULL, 11, NULL, 14),
 (11, 'Antiquaire 2', 'L\'antiquaire aimerait bien que vous trouviez le troisième vase canope.\r\n\r\n<p id =\'indice_texte\' style=\'text-align:center;\'><button class = \'bouton_indice\'>Indice</button> </p>', 'Si vous n\'avez toujours pas compris il faut regarder le nombre de bandes sur les vases.', 30.1123, 31.397, 'img/antiquaire2.png', 103, 111, NULL, 5, 14, 0, NULL, 12, NULL, 15),
 (12, 'Antiquaire 3', 'Faites attention si vous ne trouvez pas rapidement les autres vases canope, l\'antiquaire risque de refuser de vous aider.', '', 30.1123, 31.397, 'img/antiquaire3.png', 129, 111, NULL, 5, 14, 0, NULL, 13, NULL, 16),
-(13, 'Antiquaire 4', 'Bravo vous avez retrouvé tous les vases. L\'antiquaire vous indique qu\'il a entendu la femme demander comment se rendre aux pyramides qui se situent à l\'est de la ville.', '', 30.1123, 31.397, 'img/antiquaire4.png', 151, 111, NULL, 1, 14, 0, NULL, 8, NULL, NULL),
+(13, 'Antiquaire 4', 'Bravo vous avez retrouvé tous les vases. L\'antiquaire vous indique qu\'il a entendu la femme demander comment se rendre aux pyramides qui se situent à l\'ouest de la ville.', '', 30.1123, 31.397, 'img/antiquaire4.png', 151, 111, NULL, 1, 14, 0, NULL, 8, NULL, NULL),
 (14, 'Vase chacal', 'Votre inventaire est plein. Résolvez les énigmes précédentes pour pouvoir récupérer cette objet.', '', 30.1246, 31.361, 'img/vase_renard.png', 126, 167, NULL, 4, 15, 1, NULL, NULL, NULL, NULL),
 (15, 'Vase babouin', 'Votre inventaire est plein. Résolvez les énigmes précédentes pour pouvoir récupérer cet objet.', '', 30.1021, 31.3301, 'img/vase_babouin.png', 160, 83, NULL, 4, 13, 1, NULL, NULL, NULL, NULL),
 (16, 'Vase oiseau', 'Votre inventaire est plein. Résolvez les énigmes précédentes pour pouvoir récupérer cet objet.', '', 30.0658, 31.3948, 'img/vase_oiseau.png', 124, 142, NULL, 4, 15, 1, NULL, NULL, NULL, NULL),
